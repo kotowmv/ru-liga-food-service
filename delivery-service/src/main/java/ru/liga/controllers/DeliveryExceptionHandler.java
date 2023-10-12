160753b753770635f164346212d1eb3cd9639c1f
kotowmv <kotowmv@yandex.ru>
2023-10-12 17:53:12 +0300

    Hometask 3 - Add exception handler for services

---
+++ delivery-service/src/main/java/ru/liga/controllers/DeliveryExceptionHandler.java
@@ -0,0 +1,18 @@
+package ru.liga.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import java.util.NoSuchElementException;
+
+@ControllerAdvice(assignableTypes ={DeliveryController.class})
+public class DeliveryExceptionHandler {
+
+    @ExceptionHandler({NoSuchElementException.class})
+    @ResponseStatus(
+            value = HttpStatus.NOT_FOUND,
+            reason = "Requested order does not exist"
+    )
+    public void orderNotFoundException() {}
+}
